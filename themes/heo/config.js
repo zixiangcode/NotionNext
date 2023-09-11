9e5a243b6dd6f4c86e4ab46f7d73c2f819ec3de6
zixiangcode <zixiangcode@gmail.com>
2023-09-11 22:15:30 +0800

    更新 heo 主题配置

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,9 +18,9 @@ const CONFIG = {
   HERO_TITLE_LINK: 'https://github.com/zixiangcode',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '实用链接', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '往期整理', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: 'About Me', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: '实用链接', url: 'https://notion-next-henna-beta.vercel.app/links?theme=heo' },
+  HERO_CATEGORY_2: { title: '往期整理', url: 'https://notion-next-henna-beta.vercel.app/archive?theme=heo' },
+  HERO_CATEGORY_3: { title: 'About Me', url: 'https://notion-next-henna-beta.vercel.app/about?theme=heo' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HERO_RECOMMEND_POST_TAG: '推荐',
